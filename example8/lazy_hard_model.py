98c00e0153538acd99ae43fa2e641140a1813af5
Kangbb <kiloveyousmile@gmail.com>
2017-07-21 22:20:19 +0800

    improve lazy_hard_model.py

--- example8/lazy_hard_model.py
+++ example8/lazy_hard_model.py
@@ -5,7 +5,7 @@ import re
 pattern1 = 'p.*y' # 贪婪模式
 pattern2 = 'p.*?y' # 懒惰模式
 string = 'abcdfphp345Pythony_py'
-result1 = re.search(pattern1,string)
+result1 = re.search(pattern1,string, re.I)
 result2 = re.search(pattern2,string, re.I)
 print(result1)
 print(result2)
